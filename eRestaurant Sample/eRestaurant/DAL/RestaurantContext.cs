bcb9092e956e590981a215b7f9b81596e1903a61
mklause1 <mklause1@studentmail.nait.ca>
2014-09-11 09:58:46 -0600

    Added Classes and Properties for the classes

---
+++ eRestaurant Sample/eRestaurant/DAL/RestaurantContext.cs
@@ -0,0 +1,27 @@
+﻿using eRestaurant.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity; // Needed for DbContext base class
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.DAL
+{
+    class RestaurantContext : DbContext
+    {
+        // Constructor that calls a base-class constructor to specify the
+        // connection string we need to use
+        public RestaurantContext() : base("name=EatIn") { }
+
+        #region Table to Entity Mappings
+
+        public DbSet<Table> Tables { get; set; }
+        public DbSet<SpecialEvent> SpecialEvents { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+        #endregion
+
+    }//end Class
+
+}//end namespace
