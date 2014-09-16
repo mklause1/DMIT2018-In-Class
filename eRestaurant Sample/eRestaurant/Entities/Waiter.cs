e9b7a503da50a18006230b968313e8cde3e71e0c
mklause1 <mklause1@studentmail.nait.ca>
2014-09-16 15:08:00 -0600

    Added Classes and all their relations

--- eRestaurant Sample/eRestaurant/Entities/Waiter.cs
+++ eRestaurant Sample/eRestaurant/Entities/Waiter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,9 @@ namespace eRestaurant.Entities
         public string Address { get; set; }
         public DateTime HireDate { get; set; }
         public DateTime ReleaseDate { get; set; }
+
+        [NotMapped]
+        public string FullName { get { return FirstName + " " + LastName; } }
 
         #region Navigation Properties
 
