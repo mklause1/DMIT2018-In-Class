a75d76072a0541c5e32c64b78ff03128f7b270a7
mklause1 <mklause1@studentmail.nait.ca>
2014-09-16 14:04:38 -0600

    Added Classes from the Practice website

---
+++ eRestaurant Sample/eRestaurant/Entities/MenuCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Entities
+{
+    class MenuCategory
+    {
+        public int MenuCategoryID { get; set; }
+        public string Description { get; set; }
+        
+        #region Navigation Properties
+
+        public virtual ICollection<Item> Items { get; set; }
+
+        #endregion
+    }//end Class
+
+}//end namespace
