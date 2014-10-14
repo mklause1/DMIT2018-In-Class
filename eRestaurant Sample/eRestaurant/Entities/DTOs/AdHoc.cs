c1e0f1a41a751eb062ce89e76ca224c7f93ef4d1
mklause1 <mklause1@studentmail.nait.ca>
2014-10-14 14:15:58 -0600

    Added DTOs

---
+++ eRestaurant Sample/eRestaurant/Entities/DTOs/AdHoc.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Entities.DTOs
+{
+    // A delegate is a reference to a method
+    public delegate Grade GradeConverter(int percent);
+
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class AdHoc : IAdHoc // This class implements IAdHoc interface
+    {
+        //A class has fields, properties, constructors and methods
+
+        public bool IsHonours(GradeConverter callback)
+        {
+            Grade result = callback(75);
+            return IsHonours(result);
+        }
+
+        public Grade ConvertToGrade(int percent)
+        {
+            Grade result = Grade.A;
+                if (percent >= 80)
+                {
+                    result = Grade.A;
+                }//end if
+
+                return result;
+        }
+
+        public bool IsHonours(Grade value)
+        {
+            if (value == Grade.A)
+            {
+                return true;
+            }//end if
+
+            else
+            {
+                return false;
+            }//end else
+
+        }
+
+        public string AsText(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    public interface IAdHoc
+    {
+        // An interface has properties and methods
+        string name;
+        // Properties and methods do NOT have an implementation in an interface
+        string AsText(int value);
+        int Count { get; } // Properties can have a get, set or both
+    }
+
+}
---
+++ eRestaurant Sample/eRestaurant/Entities/DTOs/Order.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurant.Entities.DTOs
+{
+    public class Order
+    {
+        public int TableNumber { get; set; }
+        public string Waiter { get; set; }
+        public int WaiterID { get; set; }
+        public int? BillID { get; set; }
+        public bool Served { get; set; }
+        public string OrderComments { get; set; }
+
+        public decimal TotalAmount
+        {
+            get
+            {
+                decimal value = 0; //default value
+
+                if (Items != null)
+                {
+                    value = Items.Sum(x => x.ItemTotal); // See footnote #1
+                }//end if
+
+                return value;
+            }
+        }
+
+        public IList<OrderItem> Items { get; set; }
+    }//end Class
+
+}//end namespace
