3d34aee11a3799c9b01459fb91828c46ca65af98
mklause1 <mklause1@studentmail.nait.ca>
2014-11-06 18:00:57 -0700

    Revert "revert"
    This reverts commit 2f22969177001f688fed82f4f7c978bda3c64362.

--- eRestaurant Sample/Website/Staff/FrontDesk.aspx.cs
+++ eRestaurant Sample/Website/Staff/FrontDesk.aspx.cs
@@ -1,5 +1,4 @@
-﻿using eRestaurant.BLL;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,18 +12,5 @@ public partial class Staff_FrontDesk : System.Web.UI.Page
 
     }
 
-    protected void MockLastBillingDateTime_Click(object sender, EventArgs e)
-    {
-        MessageUserControl.TryRun(SetMockedTimeToLastBill);
-    }
-    private void SetMockedTimeToLastBill()
-    {
-        var controller = new AdHocController();
-        var info = controller.GetLastBillDateTime();
-        // formatting date for use in an <input type="date"> HTML5 control
-        SearchDate.Text = info.ToString("yyyy-MM-dd");
-        
-        // formatting time for use in an <input type="time"> HTML5 control
-        SearchTime.Text = info.ToString("HH:mm:ss"); // HH is 24 hour clock, hh is 12 hour clock
-    }
+    
 }
