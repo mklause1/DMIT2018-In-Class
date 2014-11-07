2f22969177001f688fed82f4f7c978bda3c64362
mklause1 <mklause1@studentmail.nait.ca>
2014-11-06 17:59:35 -0700

    revert

--- eRestaurant Sample/Website/Staff/FrontDesk.aspx.cs
+++ eRestaurant Sample/Website/Staff/FrontDesk.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eRestaurant.BLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,5 +13,18 @@ public partial class Staff_FrontDesk : System.Web.UI.Page
 
     }
 
-    
+    protected void MockLastBillingDateTime_Click(object sender, EventArgs e)
+    {
+        MessageUserControl.TryRun(SetMockedTimeToLastBill);
+    }
+    private void SetMockedTimeToLastBill()
+    {
+        var controller = new AdHocController();
+        var info = controller.GetLastBillDateTime();
+        // formatting date for use in an <input type="date"> HTML5 control
+        SearchDate.Text = info.ToString("yyyy-MM-dd");
+        
+        // formatting time for use in an <input type="time"> HTML5 control
+        SearchTime.Text = info.ToString("HH:mm:ss"); // HH is 24 hour clock, hh is 12 hour clock
+    }
 }
